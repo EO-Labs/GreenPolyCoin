b90106005ad4799c008816ffe71daaa5af029472
Aaron C. Bozzone <abozzone@happycoin.gq>
2022-08-16 10:26:09 +0200

    ocean vault on 2022-08-16 10:26

--- experiments/web3-wallet/app.js
+++ experiments/web3-wallet/app.js
@@ -1,6 +1,13 @@
 
 console.dir(window.web3)
+web3.personal.sign(web3.fromUtf8("Hello from Toptal!"), web3.eth.coinbase, console.log);
+
+async function main() {
+const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+console.dir(accounts)
 
 
+}
 
-console.dir(window.phantom)
+main();
