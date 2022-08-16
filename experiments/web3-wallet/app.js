937836bfd5d3ec895c919c5965cc7ebc1f2ce3c0
Aaron C. Bozzone <abozzone@happycoin.gq>
2022-08-16 11:54:54 +0200

    ocean vault on 2022-08-16 11:54

--- experiments/web3-wallet/app.js
+++ experiments/web3-wallet/app.js
@@ -1,13 +1,26 @@
 
-console.dir(window.web3)
-web3.personal.sign(web3.fromUtf8(`Hello from ${location.hostname}!`), web3.eth.coinbase, console.log);
+if (false) {
+  console.dir(window.web3)
+  web3.personal.sign(web3.fromUtf8(`Hello from ${location.hostname}!`), web3.eth.coinbase, console.log);
+}
 
-async function main() {
+!(async function main() { // assumed "ether.js" loaded
 const accounts = await ethereum.request({ method: 'eth_accounts' });
 
 console.dir(accounts)
 
+ async function web3Login() {
+        if (!window.ethereum) {
+            alert('MetaMask not detected. Please install MetaMask first.');
+            return;
+        }
+    }
 
-}
 
-main();
+  document.getElementById('login').addEventListener('click',web3Login,false);
+
+
+  async function 
+
+
+})();
